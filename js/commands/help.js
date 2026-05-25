fd6777fbe7dc35f53639534e8fe1b5ed291a7b6b
pryllejay02 <patrickantonio796@gmail.com>
2026-05-25 19:12:37 +0800

    help changes

--- js/commands/help.js
+++ js/commands/help.js
@@ -4,45 +4,49 @@ module.exports = async function helpCommand(message, prefix) {
 
 🧙 **Character**
 \`${prefix}start\` - Create character
-\`${prefix}profile\` - View profile/stats
+\`${prefix}profile\` - View stats
 \`${prefix}character\` / \`${prefix}char\` - View equipment
 
 ⚔️ **Battle**
-\`${prefix}hunt\` - Find monsters
-\`${prefix}hit\` - Attack monster
-\`${prefix}retreat\` - Escape battle
-\`${prefix}rest\` - Heal or revive
+\`${prefix}hunt\` - Find monster
+\`${prefix}hit\` - Attack
+\`${prefix}retreat\` - Escape
+\`${prefix}rest\` - Heal/revive
 
 🎒 **Inventory**
-\`${prefix}inventory\` / \`${prefix}inv\` - View items
-\`${prefix}use <item_id>\` - Use consumable
-\`${prefix}sell <item_id> <qty>\` - Sell item
-\`${prefix}sell <item_id> all\` - Sell all quantity of item
-\`${prefix}sell all\` - Sell all sellable items
-\`${prefix}sell all common\` - Sell all Common items
-\`${prefix}sell all rare\` - Sell all Rare items
+\`${prefix}inventory\` / \`${prefix}inv\`
+\`${prefix}use <item_id>\`
+\`${prefix}sell <item> <qty>\`
+\`${prefix}sell all\`
+\`${prefix}sell all common\`
+\`${prefix}sell all rare\`
 
 🏪 **Shop**
-\`${prefix}shop\` - Open shop
-\`${prefix}shop <level>\` - View level shop
-\`${prefix}buy <item_id> <qty>\` - Buy item
+\`${prefix}shop\`
+\`${prefix}shop <level>\`
+\`${prefix}buy <item> <qty>\`
 
 🛡️ **Equipment**
-\`${prefix}equip <item_id>\` - Equip item
-\`${prefix}unequip <slot>\` - Unequip item
-Slots: \`weapon\`, \`helmet\`, \`armor\`, \`gloves\`, \`pants\`, \`boots\`
+\`${prefix}equip <item>\`
+\`${prefix}unequip <slot>\`
+
+👥 **Party**
+\`${prefix}party create @player\`
+\`${prefix}party accept\`
+\`${prefix}party leave\`
+
+👹 **Boss Raid**
+\`${prefix}raid status\`
+\`${prefix}raid hit\`
 
 🏆 **Leaderboard**
-\`${prefix}leaderboard\` / \`${prefix}lb\` - View rankings
+\`${prefix}leaderboard\` / \`${prefix}lb\`
 
 🎁 **Loot**
-Lv.5+ monsters can drop equipment.
-🟢 Common, 🔵 Rare, ❌ No Drop
+🟢 Common
+🔵 Rare
+🟠 Legendary
 
-📖 **Examples**
-\`${prefix}shop 5\`
-\`${prefix}buy archer_iron_weapon 1\`
-\`${prefix}equip archer_iron_weapon\`
-\`${prefix}sell all common\`
+🔥 Explore • Hunt • Raid • Level Up
 `);
 };
